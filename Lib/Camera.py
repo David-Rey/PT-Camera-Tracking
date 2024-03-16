f68298bf9251d7711132f120ac3491d0e92828ca
David-Rey <71460537+David-Rey@users.noreply.github.com>
2024-03-15 22:15:30 -0400

    inital commit

---
+++ Lib/Camera.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Camera:
+    def __init__(self, id: str, direction: list, pos: list, fov: list):
+        self.direction = direction  # Direction the Camera is pointed
+        self.pos = pos
+        self.fov = fov
+        self.id = id
+
+        # TODO: create direction vector and position vector
+
+        # TODO:
+
+    def draw_cam(self):
+
